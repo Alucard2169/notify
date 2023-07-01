5b45ce0744456867bcb02916257c298d076e815f
Alucard2169 <ashish10deepanshu@gmail.com>
2023-07-01 13:07:39 +0530

    added prisma and database connection

--- Components/Auth.tsx
+++ Components/Auth.tsx
@@ -6,41 +6,86 @@ import {AiFillCloseCircle, AiOutlineEye, AiOutlineEyeInvisible } from "react-ico
 const Auth: FC = () => {
   const contextValue = useContext(authContext) ?? ({} as AuthContextProps);
   const { state, setState } = contextValue;
+  const [togglePassword, setTogglePassword] = useState<boolean>(false);
+  const [formContent, setFormContent] = useState<string>('login');
+  const [username, setUsername] = useState<string>('')
+  const [password, setPassword] = useState<string>('')
+  const [email, setEmail] = useState<string>('')
+  const [error,setError] = useState<string | null>(null)
+   
 
-    const [togglePassword, setTogglePassword] = useState<boolean>(false);
-    const [formContent, setFormContent] = useState<string>('login');
+   const handleUsernameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUsername(e.target.value);
+  };
+
+   const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
+
+   const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
+
 
    const handleFormContent = (): void => {
   setFormContent((prevFormContent) => (prevFormContent === 'login' ? 'sign' : 'login'));
 };
 
 
-  const handleTogglePassword = ():void => {
+const handleTogglePassword = ():void => {
     setTogglePassword(!togglePassword);
-    };
+};
     
 
     const handleAuthFormVisibility = ():void => {
       setState(false)
       setFormContent('login')
-    }
+  }
+  
 
+  const handleSignUp = async (e: React.FormEvent<HTMLFormElement>) => {
+   e.preventDefault()
+    try {
+      const response = await fetch('/api/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password,email }),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+
+        console.log(data)
+        setError(null)
+      } else {
+        // Handle login error
+        const errorData = await response.json();
+        console.log(errorData)
+        setError(errorData.error)
+      }
+    } catch (error) {
+      console.error('An error occurred during login:', error);
+    }
+  };
 
     return ( 
         <div className={`fixed  bg-black w-screen h-screen z-100 flex justify-center items-center ${!state ? "hidden" : null}`}>
             <AiFillCloseCircle className="absolute text-COMPONENT_BG text-3xl top-1/4 right-1/3 cursor-pointer hover:text-white" onClick={handleAuthFormVisibility}/>
-            <form className="flex flex-col gap-8 items-center w-1/4 border border-COMPONENT_PRIMARY_BG p-8 rounded-lg">
+        <form className="flex flex-col gap-8 items-center w-1/4 border border-COMPONENT_PRIMARY_BG p-8 rounded-lg" onSubmit={(e) => { formContent === 'sign' ? handleSignUp(e) : null }}>
+          {error && <p className="rounded-full py-1 bg-red-700 w-full text-center text-white font-semibold">{error}</p>}
                 <div className="flex flex-col gap-4 w-full">
                     <label htmlFor="username" className="text-COMPONENT_BG font-semibold flex flex-col text-base bg-MAIN w-fit px-2 rounded-sm">
                     Username
                 </label>
-                <input type="text" id="username" name="username" className="rounded-full border-none outline-none px-4 py-2"  required/>
+                <input type="text" id="username" name="username" onChange={handleUsernameChange} value={username} className="rounded-full border-none outline-none px-4 py-2"  required/>
                 </div>
                   <div className="flex flex-col gap-4 w-full relative">
                     <label htmlFor="Password" className="text-COMPONENT_BG font-semibold flex flex-col text-base bg-MAIN w-fit px-2 rounded-sm">
                     Password
                 </label>
-                    <input type={togglePassword ? 'text' : "password"} id="password" name="password" className="rounded-full border-none outline-none px-4 py-2 pr-12" required />
+                    <input type={togglePassword ? 'text' : "password"} id="password" name="password" onChange={handlePasswordChange} value={password} className="rounded-full border-none outline-none px-4 py-2 pr-12" required />
                     {togglePassword ? <AiOutlineEyeInvisible className="eyeIcon" onClick={handleTogglePassword}/> : <AiOutlineEye className="eyeIcon" onClick={handleTogglePassword}/>}
                 </div>
 
@@ -48,10 +93,11 @@ const Auth: FC = () => {
                     <label htmlFor="email" className="text-COMPONENT_BG font-semibold flex flex-col text-base bg-MAIN w-fit px-2 rounded-sm">
                     Email
                 </label>
-                <input type="email" id="email" name="email"  className="rounded-full border-none outline-none px-4 py-2" required/>
+                <input type="email" id="email" name="email" onChange={handleEmailChange} value={email} className="rounded-full border-none outline-none px-4 py-2" required/>
                 </div>
 
-                 <input type="submit" value='SUBMIT' className="rounded-full bg-COMPONENT_BG px-8 py-2 font-semibold cursor-pointer" />
+          <input type="submit" value='SUBMIT'  className="rounded-full bg-COMPONENT_BG px-8 py-2 font-semibold cursor-pointer" />
+          
                  <p className="text-white mr-auto">
   {formContent === 'sign' ? (
     <>
