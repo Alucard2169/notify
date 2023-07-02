17ccaaa51dfd2f8bc72d83ebcab02cf8e3c186e4
Alucard2169 <ashish10deepanshu@gmail.com>
2023-07-02 15:42:26 +0530

    render loading component conditionaly

--- Components/Layout.tsx
+++ Components/Layout.tsx
@@ -2,18 +2,47 @@ import React, { FC, ReactNode } from "react";
 import Navbar from "./Navbar";
 import Auth from "./Auth";
 import Loading from "./Loading";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
 
 interface LayoutProps {
   children: ReactNode;
 }
 
 const Layout: FC<LayoutProps> = ({ children }) => {
+  const [isLoading, setLoading] = useState(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleStart = () => {
+      setLoading(true);
+    };
+
+    const handleComplete = () => {
+      setLoading(false);
+    };
+
+    router.events.on("routeChangeStart", handleStart);
+    router.events.on("routeChangeComplete", handleComplete);
+    router.events.on("routeChangeError", handleComplete);
+
+    return () => {
+      router.events.off("routeChangeStart", handleStart);
+      router.events.off("routeChangeComplete", handleComplete);
+      router.events.off("routeChangeError", handleComplete);
+    };
+  }, [router]);
   return (
     <div className="bg-MAIN w-screen h-screen overflow-x-hidden">
-      <Loading/>
-          <Auth/>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <>
+          <Auth />
           <Navbar />
-      {children}
+          {children}
+        </>
+      )}
     </div>
   );
 };
