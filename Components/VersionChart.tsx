e5cde2cb853817924c1224b6f18883bcca5891f1
Alucard2169 <ashish10deepanshu@gmail.com>
2023-07-03 13:59:29 +0530

    version component with search

---
+++ Components/VersionChart.tsx
@@ -0,0 +1,114 @@
+import React, { useState } from "react";
+import { BiUpArrow, BiDownArrow } from "react-icons/bi";
+
+interface Version {
+  number: string;
+  published_at: string;
+  spdx_expression: string;
+  original_license: string;
+  researched_at: null | string;
+  repository_sources: string[];
+}
+
+const VersionChart: React.FC<{ versions: Version[] }> = ({ versions }) => {
+  const [isAscending, setIsAscending] = useState(true);
+  const [visibleVersion, setVisibleVersions] = useState(6);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const toggleOrder = () => {
+    setIsAscending(!isAscending);
+  };
+
+  const sortedVersions = isAscending
+    ? [...versions].sort(
+        (a, b) =>
+          new Date(a.published_at).getTime() -
+          new Date(b.published_at).getTime()
+      )
+    : [...versions].sort(
+        (a, b) =>
+          new Date(b.published_at).getTime() -
+          new Date(a.published_at).getTime()
+      );
+
+  const filteredVersions = sortedVersions.filter((version) =>
+    version.number.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  const truncatedVersions = filteredVersions.slice(0, visibleVersion);
+
+  const handleViewMoreVersions = (): void => {
+    setVisibleVersions(filteredVersions.length);
+  };
+
+  const handleViewLessVersions = (): void => {
+    setVisibleVersions(6);
+  };
+
+  const formattedDate = (dateString: string): string => {
+    const formattedDate = new Date(dateString).toLocaleString();
+    return formattedDate;
+  };
+
+  return (
+    <div className="relative w-1/2 bg-PRIMARY rounded-md p-4">
+      <div className="flex w-full justify-between items-center">
+        <h3 className="text-white font-semibold text-2xl">Version</h3>
+        <button onClick={toggleOrder} className="text-white mt-4">
+          {isAscending ? (
+            <BiUpArrow className="text-COMPONENT_BG text-2xl" />
+          ) : (
+            <BiDownArrow className="text-COMPONENT_BG text-2xl" />
+          )}
+        </button>
+      </div>
+      <input
+        type="text"
+        placeholder="Search version"
+        value={searchTerm}
+        onChange={handleSearch}
+        className="absolute top-20 right-4 bg-COMPONENT_BG text-MAIN font-semibold px-2 py-1 rounded-md mt-4 placeholder:text-MAIN outline-none focus:outline-white"
+      />
+      <div className="grid gap-2 mt-4">
+        {truncatedVersions.map((version) => (
+          <div
+            key={version.number}
+            className="bg-MAIN py-1 px-2 rounded-md w-1/2 flex flex-col gap-2"
+          >
+            <h5 className="text-white font-semibold text-md">
+              {version.number}
+            </h5>
+            <p className="text-COMPONENT_BG font-bold text-sm">
+              Published At:{" "}
+              <span className="text-COMPONENT_PRIMARY_BG">
+                {formattedDate(version.published_at)}
+              </span>
+            </p>
+          </div>
+        ))}
+      </div>
+      {visibleVersion < filteredVersions.length && (
+        <button
+          onClick={handleViewMoreVersions}
+          className="bg-COMPONENT_BG p-2 mt-2 w-1/2 rounded-md text-MAIN font-semibold"
+        >
+          View More
+        </button>
+      )}
+      {visibleVersion === filteredVersions.length && (
+        <button
+          onClick={handleViewLessVersions}
+          className="bg-COMPONENT_BG p-2 mt-2 w-1/2 rounded-md text-MAIN font-semibold"
+        >
+          View Less
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default VersionChart;
--- pages/technology/[platform]/[name].tsx
+++ pages/technology/[platform]/[name].tsx
@@ -1,7 +1,15 @@
 import { FC } from "react";
-import { AiOutlineGlobal, AiFillGithub } from "react-icons/ai";
-import { BiGitRepoForked } from "react-icons/bi";
+import {
+  AiOutlineGlobal,
+  AiFillGithub,
+  AiOutlineDownload,
+} from "react-icons/ai";
+import { BiGitRepoForked, BiPackage } from "react-icons/bi";
 import { FiStar } from "react-icons/fi";
+import { BsFillPeopleFill } from "react-icons/bs";
+import VersionChart from "@/Components/VersionChart";
+import { version } from "os";
+import { versions } from "process";
 
 interface TechData {
   contributions_count: number;
@@ -13,13 +21,13 @@ interface TechData {
   homepage: string; //done
   keywords: string[]; //done
   language: string; //done
-  latest_download_url: string;
-  latest_release_number: string;
-  latest_release_published_at: string;
-  latest_stable_release_number: string;
-  latest_stable_release_published_at: string;
+  latest_download_url: string; //done
+  latest_release_number: string; //done
+  latest_release_published_at: string; //done
+  latest_stable_release_number: string; //done
+  latest_stable_release_published_at: string; //done
   license_normalized: boolean;
-  licenses: string;
+  licenses: string; //done
   name: string; //done
   normalized_licenses: string[];
   package_manager_url: string;
@@ -42,11 +50,6 @@ interface versions {
   repository_sources: string[];
 }
 
-interface contextProps {
-  platform: string;
-  name: string;
-}
-
 interface TechnologyProps {
   techData: TechData;
 }
@@ -54,6 +57,12 @@ interface TechnologyProps {
 const Technology: FC<TechnologyProps> = ({ techData }) => {
   const addCommasToNumber = (number: number): string => {
     return number.toLocaleString();
+  };
+
+  const formattedDate = (dateString: string): string => {
+    const formattedDate = new Date(dateString).toLocaleString();
+
+    return formattedDate;
   };
 
   const {
@@ -68,6 +77,14 @@ const Technology: FC<TechnologyProps> = ({ techData }) => {
     forks,
     stars,
     licenses,
+    latest_release_number,
+    latest_release_published_at,
+    latest_stable_release_number,
+    latest_stable_release_published_at,
+    latest_download_url,
+    contributions_count,
+    package_manager_url,
+    versions,
   } = techData;
   return (
     <div className="py-4 px-8 flex gap-8">
@@ -77,11 +94,28 @@ const Technology: FC<TechnologyProps> = ({ techData }) => {
             <h1 className="text-3xl text-white bg-PRIMARY p-2 rounded-md w-fit font-bold">
               {name}
             </h1>{" "}
-            {homepage && (
-              <a href={homepage} target="_blank">
-                <AiOutlineGlobal className="text-blue-500 text-2xl" />
-              </a>
-            )}
+            <div className="flex gap-4 items-center">
+              {homepage && (
+                <a href={homepage} target="_blank">
+                  <AiOutlineGlobal className="text-blue-500 text-2xl" />
+                </a>
+              )}
+              {repository_url && (
+                <a href={repository_url} target="_blank">
+                  <AiFillGithub className="text-3xl text-COMPONENT_BG" />
+                </a>
+              )}
+              {latest_download_url && (
+                <a href={latest_download_url} target="_blank">
+                  <AiOutlineDownload className="text-blue-500 text-3xl" />
+                </a>
+              )}
+              {package_manager_url && (
+                <a href={package_manager_url} target="_blank">
+                  <BiPackage className="text-blue-500 text-3xl" />
+                </a>
+              )}
+            </div>
           </div>
           <span className="bg-PRIMARY p-2 rounded-md font-semibold w-fit text-COMPONENT_PRIMARY_BG">
             {language}
@@ -105,6 +139,25 @@ const Technology: FC<TechnologyProps> = ({ techData }) => {
               ))}
             </div>
           </article>
+          <ul className="flex gap-4 bg-PRIMARY p-2 w-fit rounded-md mt-2 items-center">
+            {contributions_count && (
+              <li className="bg-MAIN p-2 rounded-md flex gap-2 items-center text-white font-semibold">
+                <BsFillPeopleFill className="text-3xl text-COMPONENT_BG" />
+                {addCommasToNumber(contributions_count)}
+              </li>
+            )}
+            <li className="bg-MAIN p-2 rounded-md flex gap-2 items-center text-white font-semibold">
+              <BiGitRepoForked className="text-3xl text-COMPONENT_BG" />{" "}
+              {addCommasToNumber(forks)}
+            </li>
+            <li className="bg-MAIN p-2 rounded-md flex gap-2 items-center text-white font-semibold">
+              <FiStar className="text-3xl text-COMPONENT_BG" />
+              {addCommasToNumber(stars)}
+            </li>
+            <li className="bg-MAIN p-2 rounded-md flex gap-2 items-center text-white font-semibold">
+              License: <span className="text-COMPONENT_BG">{licenses}</span>
+            </li>
+          </ul>
           <p className="text-lg text-white font-bold">
             Platform:{" "}
             <span className="text-COMPONENT_BG font-semibold">{platform}</span>
@@ -115,30 +168,39 @@ const Technology: FC<TechnologyProps> = ({ techData }) => {
           </p>
         </section>
 
-        <ul className="flex gap-4 bg-PRIMARY p-2 w-fit rounded-md mt-2 items-center">
-          {repository_url && (
-            <li>
-              <a href={repository_url} target="_blank">
-                <AiFillGithub className="text-3xl text-COMPONENT_BG" />
-              </a>
-            </li>
-          )}
-          <li className="bg-MAIN p-2 rounded-md flex gap-2 items-center text-white font-semibold">
-            <BiGitRepoForked className="text-3xl text-COMPONENT_BG" />{" "}
-            {addCommasToNumber(forks)}
+        <ul className="mt-4 flex flex-col gap-3">
+          <h3 className="text-white font-bold text-2xl">Releases:</h3>
+          <li className="bg-PRIMARY w-full flex flex-col gap-2 p-1 rounded-md">
+            <p className="text-white font-semibold text-md">
+              Latest Stable Release Number:{" "}
+              <span className="text-COMPONENT_BG py-1 px-2 bg-MAIN rounded-md">
+                {latest_stable_release_number}
+              </span>
+            </p>
+            <p className="text-white font-semibold text-md">
+              Published At:{" "}
+              <span className="text-COMPONENT_BG py-1 px-2 bg-MAIN rounded-md">
+                {formattedDate(latest_stable_release_published_at)}
+              </span>{" "}
+            </p>
           </li>
-          <li className="bg-MAIN p-2 rounded-md flex gap-2 items-center text-white font-semibold">
-            <FiStar className="text-3xl text-COMPONENT_BG" />
-            {addCommasToNumber(stars)}
-          </li>
-          <li className="bg-MAIN p-2 rounded-md flex gap-2 items-center text-white font-semibold">
-            License: <span className="text-COMPONENT_BG">{licenses}</span>
+          <li className="bg-PRIMARY w-full flex flex-col gap-2 p-1 rounded-md">
+            <p className="text-white font-semibold text-md">
+              Latest Release Number:{" "}
+              <span className="text-COMPONENT_BG py-1 px-2 bg-MAIN rounded-md">
+                {latest_release_number}
+              </span>
+            </p>
+            <p className="text-white font-semibold text-md">
+              Published At:{" "}
+              <span className="text-COMPONENT_BG py-1 px-2 bg-MAIN rounded-md">
+                {formattedDate(latest_release_published_at)}
+              </span>{" "}
+            </p>
           </li>
         </ul>
       </div>
-      <div className="w-1/2 bg-PRIMARY rounded-md p-4">
-        <p>Version</p>
-      </div>
+      <VersionChart versions={versions} />
     </div>
   );
 };
