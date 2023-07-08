ab5a21bd88b9f86c50c6a4650520c9780d244a33
Alucard2169 <ashish10deepanshu@gmail.com>
2023-07-08 21:05:06 +0530

    removed error code from next.js.config

--- next.config.js
+++ next.config.js
@@ -1,25 +1,6 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
-  webpack: (config, { isServer }) => {
-    // Enable tree shaking
-    config.optimization.minimize = true;
-    config.optimization.minimizer[0].options.terserOptions = {
-      // Enable tree shaking for production build
-      compress: {
-        pure_funcs: ["console.log"],
-      },
-    };
+}
 
-    // Exclude server-side specific packages from client-side bundle
-    if (!isServer) {
-      config.resolve.fallback = {
-        fs: false,
-        module: false,
-        net: false,
-        child_process: false,
-      };
-    }
-
-    return config;
-  },
-};
+module.exports = nextConfig
