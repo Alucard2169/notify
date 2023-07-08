b790dadeaa79424b6687f41e1257a5f5cf322e84
Alucard2169 <ashish10deepanshu@gmail.com>
2023-07-08 20:57:52 +0530

    added some optimization

--- next.config.js
+++ next.config.js
@@ -1,6 +1,25 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   reactStrictMode: true,
-}
+  webpack: (config, { isServer }) => {
+    // Enable tree shaking
+    config.optimization.minimize = true;
+    config.optimization.minimizer[0].options.terserOptions = {
+      // Enable tree shaking for production build
+      compress: {
+        pure_funcs: ["console.log"],
+      },
+    };
 
-module.exports = nextConfig
+    // Exclude server-side specific packages from client-side bundle
+    if (!isServer) {
+      config.resolve.fallback = {
+        fs: false,
+        module: false,
+        net: false,
+        child_process: false,
+      };
+    }
+
+    return config;
+  },
+};
--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,11 @@
+const purgecss = require("@fullhuman/postcss-purgecss")({
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+});
+
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+  plugins: [
+    "tailwindcss",
+    ...(process.env.NODE_ENV === "production" ? [purgecss] : []),
+  ],
+};
