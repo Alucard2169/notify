eb50a03bf146ffd0b9e86eba7807c1a4801b6c7e
Alucard2169 <ashish10deepanshu@gmail.com>
2023-07-08 21:06:57 +0530

    fixed postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,6 @@
-const purgecss = require("@fullhuman/postcss-purgecss")({
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-});
-
 module.exports = {
-  plugins: [
-    "tailwindcss",
-    ...(process.env.NODE_ENV === "production" ? [purgecss] : []),
-  ],
-};
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
